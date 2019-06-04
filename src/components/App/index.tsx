bfb58ef97d8746d372830ca5fb02d3fe12894391
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-04 11:44:42 +0900

    add RootContext Provider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import RootStore from "@/stores/RootStore";
+
 import Header from "@/components/layouts/Header";
 import Main from "@/components/layouts/Main";
 
+export const RootContext = React.createContext({} as RootStore);
+
 export default () => (
-  <>
+  <RootContext.Provider value={new RootStore()}>
     <Header />
     <Main />
-  </>
+  </RootContext.Provider>
 );
