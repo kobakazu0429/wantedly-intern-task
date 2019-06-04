57bacc838a92f50b78141f5faf7cd8fda319469f
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-04 11:03:35 +0900

    fix stores

--- src/stores/ToDoStore.ts
+++ src/stores/ToDoStore.ts
@@ -1,7 +1,19 @@
-import ToDoModel, { IToDo } from "../models/ToDoModel";
+import ToDo, { IToDo } from "@/models/ToDo";
+import { useLocalStore } from "mobx-react-lite";
 
-export default class ToDoStore {
-  public createToDo(json: IToDo) {
-    return new ToDoModel(this, json);
-  }
+export interface IToDoStore {
+  todos: IToDo[];
+  addTodo: (text: string) => void;
+  deleteTodo: (uuid: string) => void;
 }
+
+export default () =>
+  useLocalStore<IToDoStore>(() => ({
+    todos: [],
+    addTodo(text: string) {
+      this.todos.push(ToDo(text));
+    },
+    deleteTodo(uuid: string) {
+      this.todos = this.todos.filter(todo => todo.uuid !== uuid);
+    }
+  }));
