ef645b4de501080fa5fe566a8a6592095cd8fbd4
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-04 23:21:36 +0900

    fix design

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -11,22 +11,24 @@ interface IProps {
 export default (props: IProps) => {
   return props.isShowing ? (
     <>
-      <Wrapper isShowing={props.isShowing}>{props.children}</Wrapper>
+      <Wrapper>{props.children}</Wrapper>
       <BackGround isShowing={props.isShowing} />
     </>
   ) : null;
 };
 
-const Wrapper = styled.div<{ isShowing: boolean }>`
+const Wrapper = styled.div`
   width: 60vw;
   height: 70vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0 auto;
   background-color: white;
   transition: opacity ease 500ms;
-  position: relative;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   z-index: ${zIndex.MODAL};
 
   @media ${device.mobile} {
